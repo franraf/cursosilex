309d7ddc77e59f6e596e7f688f0884a7b650f0e8
rafael <francois.rafa@gmail.com>
2017-09-06 18:31:25 -0300

    inicio

--- public/index.php
+++ public/index.php
@@ -3,7 +3,8 @@
 require_once __DIR__.'/../bootstrap.php';
 
 use Symfony\Component\HttpFoundation\Response;
-use Silex\Cliente;
+use Silex\Cliente\Cliente;
+use Silex\Cliente\ClienteAggregator;
 
 $response = new Response();
 
@@ -18,7 +19,17 @@ $app->get("/ola/{nome}", function($nome) {
 
 $app->get("/clientes", function() {
     $clientes = new ClienteAggregator();
-    foreach ($clientes as $cliente)
+    $cli1 = new Cliente();
+    $cli2 = new Cliente();
+    $cli1->setNome("Rafa1");
+    $cli1->setEmail("rafa1@rafa.com");
+    $cli1->setCpf(12345678901);
+    $cli2->setNome("Rafa2");
+    $cli2->setEmail("rafa2@rafa.com");
+    $cli2->setCpf(23456789012);
+    $clientes->addCliente($cli1);
+    $clientes->addCliente($cli2);
+    foreach ($clientes->getClientes() as $cliente)
     {
         json_encode($cliente);
     }
