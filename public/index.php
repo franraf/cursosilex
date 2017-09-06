93020f2c080e535a8f33b3dec5875c2710fc9978
rafael <francois.rafa@gmail.com>
2017-09-06 18:23:25 -0300

    inicio

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 require_once __DIR__.'/../bootstrap.php';
 
 use Symfony\Component\HttpFoundation\Response;
+use Silex\Cliente;
 
 $response = new Response();
 
@@ -15,8 +16,12 @@ $app->get("/ola/{nome}", function($nome) {
     return "Ola {$nome}";
 });
 
-$app->get("/cliente", function() {
-
+$app->get("/clientes", function() {
+    $clientes = new ClienteAggregator();
+    foreach ($clientes as $cliente)
+    {
+        json_encode($cliente);
+    }
 });
 
 $app->run();
