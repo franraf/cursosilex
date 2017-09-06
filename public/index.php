b3dcf532967120933fee09c0dfe9d20952fc3c3f
rafael <francois.rafa@gmail.com>
2017-09-06 20:06:49 -0300

    inicio

--- public/index.php
+++ public/index.php
@@ -3,8 +3,18 @@
 require_once __DIR__.'/../bootstrap.php';
 
 use Symfony\Component\HttpFoundation\Response;
+use Code\Sistema\Service\ClienteService;
+use Code\Sistema\Mapper\ClienteMapper;
+use Code\Sistema\Entity\Cliente;
 
 $response = new Response();
+
+$app['clienteService'] = function() {
+    $clienteEntity = new Cliente();
+    $clienteMapper = new ClienteMapper();
+    $clienteService = new ClienteService($clienteEntity, $clienteMapper);
+    return $clienteService;
+};
 
 $app->get("/", function() use ($response){
     $response->setContent("Ola mundo!");
@@ -21,6 +31,16 @@ $app->get("/clientes", function() {
     $post_data = array($post_data1, $post_data2);
     $json = json_encode($post_data);
     return $json;
+});
+
+$app->get("/cliente", function () use ($app) {
+   $dados['nome'] = 'Rafa';
+   $dados['email'] = 'rafa@rafa.com';
+   $dados['cpf'] = '12345678901';
+
+   $result = $app['clienteService']->insert($dados);
+
+   return $app->json($result);
 });
 
 $app->run();
---
+++ src/Code/Sistema/Service/ClienteService.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Code\Sistema\Service;
+
+use Code\Sistema\Entity\Cliente;
+use Code\Sistema\Mapper\ClienteMapper;
+
+class ClienteService
+{
+    private $cliente;
+    private $clienteMapper;
+
+    public function __construct(Cliente $cliente, ClienteMapper $clienteMapper)
+    {
+        $this->cliente = $cliente;
+        $this->clienteMapper = $clienteMapper;
+    }
+
+    public function insert(array $data)
+    {
+        $cliente = $this->cliente;
+        $cliente->setNome($data['nome']);
+        $cliente->setEmail($data['email']);
+        $cliente->setCpf($data['cpf']);
+
+        $mapper = $this->clienteMapper;
+        $result = $mapper->insert($cliente);
+
+        return $result;
+    }
+}
